ca619ab8ff040a059a9f094e23a9098b1f798422
Eduard Krasnov <eduard.krasnov1@hm.edu>
2022-12-15 09:47:12 +0100

    Navbar startpage und contentpage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,15 @@ import "./Navbar.css"
 import LanguageIcon from '@mui/icons-material/Language';
 import SearchIcon from '@mui/icons-material/Search';
 import logoShort from "../../logo_short.svg"
+import { useState } from "react";
+
 
 const Navbar = () => {
-    return ( 
-        //Navbar search aus
+    const [viewStatus, setViewStatus] = useState("startPage");
+
+            //Navbar search aus
+    if (viewStatus==="startPage"){  
+        return (       
         <div className="navbar">
             <div className="icon--button">
             <LanguageIcon sx={{ fontSize: 32}}/>
@@ -17,10 +22,24 @@ const Navbar = () => {
             <SearchIcon sx={{ fontSize: 32}}/>
             </div>
         </div>
-
-        //Navbar search ein
-        
-     );
+        );
+    }
+            //Navbar search ein
+    else if (viewStatus==="contentPage"){
+        return (       
+            <div className="navbar">
+                <div className="icon--button" id="language">
+                <LanguageIcon sx={{ fontSize: 32}}/>
+                </div>
+                <div className="navbar--logo">
+                    <img src={logoShort} alt="Navbar logo" width="50px" height="25px"/>
+                </div>
+                <div className="icon--button"  id="search">
+                <SearchIcon sx={{ color: "white" }}/> 
+                </div>
+            </div>
+            );
+    }
 }
  
 export default Navbar;
