e4469f80e53b0a35b092e68b8ed374c15416265c
Lea Zeilbeck <lea.zeilbeck@googlemail.com>
2022-12-15 20:53:03 +0100

    Card und Card Page unfertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import ContentCard from './components/ContentCard/ContentCard';
 import Navbar from "./components/Navbar/Navbar";
+import Card from './components/ContentCard/ContentCard';
+import Card_Page from "./components/ContentPage/ContentPage"
 
 const App = () => {
   return ( 
-    <Navbar />
-   );
+    <div className="App">
+        <Navbar
+        />
+        <Card
+        />
+        <Card_Page
+        />
+
+    </div>
+    );
+
 }
  
 export default App;
