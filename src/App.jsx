5188a074e160c0486c04d4c79f43afb0585125ad
Lea Zeilbeck <lea.zeilbeck@googlemail.com>
2022-12-16 14:11:21 +0100

    ContentPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import ContentCard from './components/ContentCard/ContentCard';
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { appTheme } from "./themes/theme.ts";
 import Navbar from "./components/Navbar/Navbar";
 import ContentPage from "./components/ContentPage/ContentPage";
 import FilterMenu from "./components/FilterMenu/FilterMenu";
+import ContentInspect from "./components/ContentInspect/ContentInspect";
 
 const App = () => {
   return ( 
@@ -13,7 +13,7 @@ const App = () => {
 
     <div className="app">
     <Navbar />
-    <ContentPage />
+    <ContentInspect />
     </div>
 
     </ThemeProvider>
--- src/components/ContentInspect/ContentInspect.jsx
+++ src/components/ContentInspect/ContentInspect.jsx
@@ -1,48 +1,38 @@
-import "./ContentPage.css"
-import logoShort from "../../logo_short.svg"
+import "./ContentInspect.css"
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
 
-const ContentPage = () => {
-   
-        return (       
-        <div className="content--page">
-            {/* <img className="card-image">C:\Users\leaze\OneDrive\Desktop\onleisure\src\components\ContentCard\regular-16-9.jfif</img> */}
-
-            <div className = "Platzhalter">
-                <img src="https://images.unsplash.com/photo-1670787325681-cc481822bbe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60" alt="Beispiel Bild"/>
+const ContentInspect = () => {
+    return ( 
+        <div className="inspectPage">
+            <div className="image-wrapper">
+                <img className="image" src="https://th.bing.com/th/id/OIP.-XS8esOqgS6IksKfObYnkAHaEK?pid=ImgDet&rs=1" alt="Beispiel Bild"/>
             </div>
-
-            <div className="frame">
-            
-                <div className="name-wrapper">
-                <p className="name">Fußball...</p>
+            <div className="content">
+                <div className="titel">
+                    <p>Fußball Training - FC Teutonia</p>
                 </div>
-
-                <div className="info">
-                    <p className="infoText">Datum-Uhrzeit</p>
-
-                    <p className="infoText">Blablastraße 11</p>
+                <div className="info-contentInspect">
+                    <p>24.12.2022 - 15Uhr</p>
+                    <p>Schwere-Reiter-Str. 13, 80637</p>
                 </div>
-
-                <div className="filterbox">
-                    <p className="filterbox--text">Outdoor</p>
+                <div className="kategorienLeiste">
+                    <p className="kategorie">Fußball</p>
+                    <p className="kategorie">18-26 J</p>
+                    <p className="kategorie">Outdoor</p>
                 </div>
-
-                <div className="infoText-wrapper">
-                <p className="infoText">Ich bin ein Infotext...</p>
-                </div>
-
+                <p className="infotext">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et </p>
                 <div className="kontakt">
-                    <div className="telefon">
-                        <p className="kontaktText">Telefonnummer</p>
-                    </div>
-                    <div className="email">
-                        <p className="kontaktText">Telefonnummer</p>
-                    </div>
+                    <LocalPhoneIcon sx={{ fontSize: 32}}/>
+                    <p className="kontaktText">0162/823193223</p>
+                </div>
+                <div className="kontakt">
+                    <EmailIcon sx={{ fontSize: 32}}/>
+                    <p className="kontaktText">max.mustermann@gmx.de</p>
                 </div>
             </div>
         </div>
-        );
+     );
 }
-
  
-export default ContentPage;
+export default ContentInspect;
