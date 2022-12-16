a08967b92e767a449e40552616e4282e764e4a50
Lea Zeilbeck <lea.zeilbeck@googlemail.com>
2022-12-16 14:10:07 +0100

    ContentInspekt

---
+++ src/components/ContentPage/ContentPage.jsx
@@ -0,0 +1,16 @@
+import "./ContentPage.css"
+import ContentCard from '../ContentCard/ContentCard';
+
+
+const ContentPage = () => {
+    return ( 
+      <div className="app">
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      </div>
+     );
+  }
+   
+  export default ContentPage;
