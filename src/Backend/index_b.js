6b361c742d37e4d249f7f231eb3b6e017b51343b
Lea Zeilbeck <lea.zeilbeck@googlemail.com>
2023-01-11 09:52:53 +0100

    Backend

---
+++ src/Backend/index_b.js
@@ -0,0 +1,90 @@
+import express from 'express';
+//import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+import Offer from "./models/offer";
+import process from 'node:process';
+
+const app = express();
+const port = process.env.PORT || 80;
+// const uri = `mongodb+srv://LeaScal:5bFHraQUJtK0mRDW@backendprojekt.kmmdivn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb://localhost:27017/test';
+
+mongoose.set('strictQuery', true); 
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("db started")
+
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+connectToDB()
+app.use(express.json());
+
+//Endpoints Offer
+app.get('/offers', async (req, res) => { 
+    try
+    {
+        const results = await Offer.find({});
+        console.log(results);
+        return res.status(200).send(results);
+    } catch (error)
+    {
+        res.status(500).send(error.message);
+    }   
+  });
+
+app.get('/offer/:id', async (req, res) => {
+    try
+    {
+        const id = req.params.id;
+        const results = await Offer.findById(id);
+        console.log(results);
+        return res.status(200).send(results);
+    } catch (error)
+    {
+        res.status(500).send(error.message);
+    }
+    });
+  
+app.post('/OfferCreate', async (req, res) => {
+    try 
+    {
+        const offer = new Offer( req.body )
+        await offer.save()
+        res.status(200).send(offer)
+    } catch(error) 
+    {
+        res.status(500).send(error.message);
+    }
+  })
+
+  app.patch('/offer', async (req, res) => { 
+    try
+    {
+        await Offer.findByIdAndUpdate(req.body,req.body,{runValidators: true});
+        res.status(200).send("OK");
+    } 
+    catch(error) 
+    {
+    res.status(500).send(error.message);
+    }
+    })
+
+app.delete('/offer/:id', async (req, res) => { 
+    try
+    {
+        const id = req.params.id;
+        const results = await Offer.deleteOne({ _id: id });
+        console.log(results);
+        res.status(200).send("OK");
+    } 
+    catch(error) 
+    {
+    res.status(500).send(error.message);
+    }
+    })
+
---
+++ src/Backend/models/offer.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import Validator from 'validator';
+
+const { Schema } = mongoose;
+const validator = Validator;
+
+const OfferSchema = new Schema({
+
+    title:  {  type: String,
+        required: true },
+
+    discription:  {  type: String,
+        required: true
+    },
+   
+    Image:  {  type: Image,
+        required: true
+    },
+    adress:  {  type: String,
+            required: true
+        },
+           
+    time :    {  type: String,
+            required: true,
+            validate: {
+                validator: time => validator.isDate(time),
+                message: 'time is not valid.'
+              }
+         }, 
+
+    age:  {  type: Number,
+            required: true,
+            min: 0,
+            max: 120
+        },    
+    
+    location_Outside: {
+                type: Boolean,
+                required: true
+    },
+    location_Inside: {
+        type: Boolean,
+        required: true
+},
+    eMail: {  type: String,
+            required: true,
+            validate: {
+                validator: email => validator.isEmail(email),
+                message: 'eMail is not valid.'
+              }},
+
+    telefon:  {
+                type: String,
+                validate: {
+                    validator: num => validator.isMobilePhone(num),
+                    message: 'Phonenumber is not valid.'
+                  }
+              },
+    activity:  {  type: String,
+                required: true },
+});
+
+
+
+const Offer = mongoose.model('Offer', OfferSchema)
+export default Offer;
