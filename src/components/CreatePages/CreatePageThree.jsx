c815e674cc5239e20fb6b40de116692aa21e569b
Eduard Krasnov <eduard.krasnov1@hm.edu>
2023-01-19 17:41:45 +0100

    datepicker funktionier oder so

--- src/components/CreatePages/CreatePageThree.jsx
+++ src/components/CreatePages/CreatePageThree.jsx
@@ -6,12 +6,14 @@ import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import * as Yup from 'yup';
 import React from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { LocalizationProvider, DateTimePicker} from "@mui/x-date-pickers";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import {TextField} from '@mui/material';
+import DatePickerField from "./DatePickerField";
 
 const CreatePageThree = ({navType, onNavTypeChange, progressValue, onProgressValueChange, createValues, onCreateValuesChange}) => {
     const [toValueCreate, setToValueCreate] = useState(false);
-console.log(createValues);
+    console.log(createValues);
     
     useEffect(() => {
         onNavTypeChange("createPage");
@@ -20,9 +22,9 @@ console.log(createValues);
       return ( 
         <div className="createPages">
             {toValueCreate && <Navigate to="/create/4"/>}
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
             <Formik
             initialValues={{ 
-                startDate: new Date(),
                 adress: createValues.adress,
                 city: createValues.city 
             }}
@@ -35,8 +37,6 @@ console.log(createValues);
                 city: Yup.string()
                 .max(15, 'Der Stadtname ist zu lang!')
                 .required('Bitte angeben'),
-
-                date: Yup.date().required('Bitte angeben')
                 })}
 
                 onSubmit={(values, { setSubmitting }) => {
@@ -75,30 +75,22 @@ console.log(createValues);
                     </div>
 
                     <div className="DatumAbfrage">
-                       
-                    <DatePicker 
-                      selected={values.startDate}
-                      dateFormat="MMMM d, yyyy"
-                      className="form-control"
-                      name="startDate"
-                      onChange={date => setFieldValue('startDate', date)}
-                    />
-                   
+                        <Field  
+                            component={DatePickerField}
+                            className="datetimepicker"
+                            name="datetime"
+                            label="Start Zeit"
+                            />
                     </div>
-
-                    <div className="ZeitAbfragen">
-                    <Field className="standortEingabefeld" name="time" type="text" placeholder="z.B 18:00 - 20:00" />
-                        <ErrorMessage name="time" />
-                    </div>
-
-                <div className="buttonCreate3">
-                <Button version="dick" isDisabled={false}>Weiter</Button>
+                <div className="button-wrapper">
+                    <Button type="submit" version="dick" isDisabled={false}>Weiter</Button>
                 </div>
                 </div>
             </Form>
         </div>
         )}
             </Formik>
+            </LocalizationProvider>
             </div>
      );}
 export default CreatePageThree;
